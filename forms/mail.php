f250bd0e334b811fab953d026e48d5302a7a5203
prestoneadie <prestoneapogee@gmail.com>
2023-10-30 16:37:44 +0300

    trying to fix form submit

--- forms/mail.php
+++ forms/mail.php
@@ -1,17 +1,21 @@
 <?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$subject = $_POST['subject'];
-$phonenumber = $_POST['phonenumber'];
-$message = $_POST['message'];
+if (isset($_POST['sendersend'])){
+    $name = $_POST["sendername"];
+    $email = $_POST["senderemail"];
+    $subject = $_POST["sendersubject"];
+    $phonenumber = $_POST["senderphonenumber"];
+    $message = $_POST["sendermessage"];
 
-$mailheader = "From: ".$name."< provided".$phonenumber."and".$email.">\r\n";
+    $mailheader = "From: ". $name ."< provided". $phonenumber ."and". $email .">\r\n";
 
-$recipient = "hello@prestonead.ie";
+    $recipient = "hello@prestonead.ie";
 
-mail($recipient, $subject, $message, $mailheader) 
-or die("Error!");
+    // mail($recipient, $subject, $message, $mailheader) 
+    // or die("Error!");
 
-echo'Your Email has been sent! I will get back to you as soon as possible';
+    // echo "Your Email has been sent! I will get back to you as soon as possible";
+
+    echo $message;
+}
 
 ?>
